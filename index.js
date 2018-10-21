561250bcf1b68aec9d11b833bebcf443836999cb
evgenyFinkin <jon.finkin@gmail.com>
2018-10-21 23:43:29 +0300

    restart

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 'use strict';
 
-  
-const express = require("express"),
-      fileUpload = require("express-fileupload"),
-      app = express();
-
 var fs = require('fs');
 var path = require('path');
 
-app.use(fileUpload());
-
-app.use(express.static("public"));
-
-app.post('/upload', function(req, res) {
-  if (Object.keys(req.files).length == 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
-
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  const sampleFile = req.files.fileToUpload;
-
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
-    if (err)
-      return res.status(500).send(err);
-
-    res.send('File uploaded!');
-  });
-});
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
@@ -38,4 +13,3 @@ exports.get = function(event, context, callback) {
   };
   callback(null, result);
 };
-app.listen(3000);
